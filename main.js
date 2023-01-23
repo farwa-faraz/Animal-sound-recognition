b926110d4a6336bfa0ee874d85a0f64c41389813
farwa-faraz <99012312+farwa-faraz@users.noreply.github.com>
2023-01-23 16:55:49 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function Classify()
 {
     navigator.mediaDevices.getUserMedia({ audio: true});
-    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/U7EPU-xaz/model.json", modelReady);
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/S6WVyyHof/model.json", modelReady);
 }
 
 function modelReady()
 {
     console.log("Your model is ready");
-    classifier.classify(gotResult);
+    classifier.classify(gotResults);
 }
 
-function gotResult(error, results)
+function gotResults(error, results)
 {
     if(error)
     {
@@ -22,5 +22,37 @@ function gotResult(error, results)
 
         document.getElementById("result_1").innerHTML = "I can hear - "+ results[0].label;
         
+        img1 = document.getElementById('image1')
+
+
+        if(results[0].label == "Background Noise")
+        {
+            img1.src = 'Main Img.gif';
+            console.log("Background Noise");
+        }
+
+        else if(results[0].label == "Mooing")
+        {
+            img1.src = 'cow.png';
+            console.log("Mooing");
+        }
+
+        else if(results[0].label == "Meowing") 
+        {
+            img1.src = 'cat.png';
+            console.log("Meowing");
+        }
+
+        else if(results[0].label == "Roar") 
+        {
+            img1.src = 'tiger.png';
+            console.log("Roar");
+        }
+
+        else //Barking
+        {
+            img1.src = 'dog.png';
+            console.log("Barking");
+        }
     }
 }
